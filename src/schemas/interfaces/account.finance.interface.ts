86d387aa777e31e4704b43b40d7cc5ff4e2b1071
flareis <flavia.reis@dasa.com.br>
2023-04-30 17:19:05 -0300

    feature: created interface, schema and DTO for account finance

---
+++ src/schemas/interfaces/account.finance.interface.ts
@@ -0,0 +1,9 @@
+import { ProductsEnum } from './enums/products.enum';
+import { IError } from './error.interface';
+
+export interface IAccount {
+  account_id: number;
+  limit: number;
+  products: [ProductsEnum]; // ou string[];
+  errors?: IError[];
+}
