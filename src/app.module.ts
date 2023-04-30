05d6ce65b036af2956aab3447e55173d33d52fa0
flareis <flavia.reis@dasa.com.br>
2023-04-30 17:21:42 -0300

    feature: inclusion of dependencies

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AccountFinanceModule } from './account-finance.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AccountFinanceController } from './controllers/account-finance.controller';
+import { AccountFinanceService } from './services/account-finance.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost:27017/Analytics', {
+      connectionName: 'Accounts',
+    }),
+    AccountFinanceModule,
+  ],
 })
 export class AppModule {}
