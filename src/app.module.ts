a4c8a5a0093152d323486ec6fc35467bac3b36b8
flareis <flavia.reis@dasa.com.br>
2023-05-12 17:18:57 -0300

    feature: update app.module whit new dependencies

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,16 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { AccountFinanceModule } from './account-finance.module';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { AccountFinanceController } from './controllers/account-finance.controller';
-import { AccountFinanceService } from './services/account-finance.service';
+import { FinanceModule } from './finance.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost:27017/Analytics', {
       connectionName: 'Accounts',
     }),
-    AccountFinanceModule,
+    MongooseModule.forRoot('mongodb://localhost:27017/Analytics', {
+      connectionName: 'Transactions',
+    }),
+    FinanceModule,
   ],
 })
 export class AppModule {}
