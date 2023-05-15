67439ee5ccb953c0460b3908a2c54aa6fb3b3c78
flareis <flavia.reis@dasa.com.br>
2023-05-15 15:49:23 -0300

    feature: changed AccountFinance for number

--- src/dto/creat-transaction-finance.dto.ts
+++ src/dto/creat-transaction-finance.dto.ts
@@ -1,8 +1,8 @@
-import { AccountFinance } from 'src/schemas/account.finance.schema';
+import { ITransactionFinance } from 'src/schemas/interfaces/transaction-finance.interface';
 import { ITransactions } from 'src/schemas/interfaces/transactions.interface';
 
-export class CreateTransactionFinanceDto {
-  readonly account_id: AccountFinance;
+export class CreateTransactionFinanceDto implements ITransactionFinance {
+  readonly account_id: number;
   readonly transaction_count: number;
   readonly bucket_start_date: Date;
   readonly bucket_end_date: Date;
