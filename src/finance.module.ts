72fec7fdbf2e0a70c7403807311290f24f7d451b
flareis <flavia.reis@dasa.com.br>
2024-01-12 19:52:52 -0300

    feat: inclusion of dependences in finace.module

--- src/finance.module.ts
+++ src/finance.module.ts
@@ -1,18 +1,25 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AccountFinanceController } from './controllers/account-finance.controller';
+import { CustomersFinanceController } from './controllers/costumers-finance.controller';
 import { TransactionFinanceController } from './controllers/transaction-finance.controller';
 import { AccountMongoRepository } from './repository/account.finance.repository';
+import { CustomersMongoRepository } from './repository/customers.finance.repository';
 import { TransactionMongoRepository } from './repository/transaction-finance.repository';
 import {
   AccountFinance,
   AccountFinanceSchema,
 } from './schemas/account.finance.schema';
 import {
+  CustomersFinance,
+  CustomersFinanceSchema,
+} from './schemas/customers.finance.schema';
+import {
   TransactionFinance,
   TransactionFinanceShema,
 } from './schemas/transaction-finance.schema';
 import { AccountFinanceService } from './services/account-finance.service';
+import { CustomersFinanceService } from './services/customers-finance.service';
 import { TransactionFinanceService } from './services/transaction-finance.service';
 
 @Module({
@@ -30,13 +37,23 @@ import { TransactionFinanceService } from './services/transaction-finance.servic
       'Transactions',
     ),
   ],
-  controllers: [AccountFinanceController, TransactionFinanceController],
+  controllers: [
+    AccountFinanceController,
+    TransactionFinanceController,
+    CustomersFinanceController,
+  ],
   providers: [
     AccountFinanceService,
     AccountMongoRepository,
     TransactionFinanceService,
     TransactionMongoRepository,
+    CustomersFinanceService,
+    CustomersMongoRepository,
   ],
-  exports: [AccountFinanceService, TransactionFinanceService],
+  exports: [
+    AccountFinanceService,
+    TransactionFinanceService,
+    CustomersFinanceService,
+  ],
 })
 export class FinanceModule {}
